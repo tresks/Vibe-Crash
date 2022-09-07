3b0ef6d329ec447c124908a0005f5de91add2801
tresks <55102988+tresks@users.noreply.github.com>
2022-09-07 20:09:23 +0100

    Update crash.lua

--- crash.lua
+++ crash.lua
@@ -1,5 +1,4 @@
 repeat wait() until game:IsLoaded()
-syn.queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/tresks/Vibe-Crash/main/crash.lua"))()')
 if isfile("webhookfortresk.txt") then
     _G.advert = false
     _G.sexycrash = true
